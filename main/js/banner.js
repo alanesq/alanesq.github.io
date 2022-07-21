42ca0004b3abe5a202dd35f19d4533fcbc64d69b
Alan <60433566+alanesq@users.noreply.github.com>
2022-07-21 11:39:21 +0100

    Update banner.js

--- main/js/banner.js
+++ main/js/banner.js
@@ -1,9 +1,9 @@
 
-// Shows banner if the image file exists - Dec 19
+// Shows banner if the image file exists - 21Jul22
 
 
 
-let fName = "http://www.alanesq.eu5.net/banner.gif";
+let fName = "https://alanesq.github.io/main/banner.gif";
 
 
 
@@ -14,7 +14,7 @@ theImg.src = fName;
 theImg.onload = function () {
     // successfully loaded so do something here.
     document.getElementById('bannerDiv').innerHTML = 
-        "<a target='_blank' href='http://www.alanesq.com/banner.htm'>" + 
+        "<a target='_blank' href='https://alanesq.github.io/main/banner.htm'>" + 
         "<img width='100%' src='" + fName + "'></a>";
 }
 
