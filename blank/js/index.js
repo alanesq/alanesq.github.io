ac2c1aaf9b6ac446e497265807e18fd33e3ca6c5
Alan <60433566+alanesq@users.noreply.github.com>
2022-08-18 16:33:06 +0100

    blank web page

---
+++ blank/js/index.js
@@ -0,0 +1,13 @@
+
+//            Javascript sheet for index.htm - 03 Nov 19 
+
+//            reference: https://www.w3schools.com/jsref/default.asp
+
+
+
+// ------------------------------ main settings ------------------------ 
+
+
+
+
+// ---------------------------------- e n d ---------------------------- 
